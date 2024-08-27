7425936ed3f229d864c07bd2251ea3dce58e5278
Rajasree Laha <laharajasree121@gmail.com>
2024-08-27 10:07:31 +0530

    Add files via upload

---
+++ New folder/printed out/armstrong with power and using function.cpp
@@ -0,0 +1,34 @@
+/*armstrong no using function without using power*/
+#include<stdio.h>
+int armstrong(int);
+int power(int);
+int main()
+{
+	int n,k;
+	printf("enter the value of n\n");
+	scanf("%d",&n);
+	k=armstrong(n);
+	if(k==n)
+	printf("armstrong no");
+	else
+	printf("not armstrong no");
+	return 0;
+}
+int armstrong(int x)
+{
+	int sum=0,rem;
+	while(x!=0)
+	{
+		rem=x%10;
+		sum=sum+power(rem);
+		x=x/10;
+	}
+	return sum;
+}
+int power(int y)
+{
+	int c;
+	c=(y*y*y);
+	return c;
+}
+
